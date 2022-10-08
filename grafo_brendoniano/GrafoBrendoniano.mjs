163fd4d62f0cd1d9de941405006cdbfa952f3397
brendoLoR <brendoja@gmail.com>
2022-10-08 00:05:07 -0300

    ajustes no codigo e evolução na inferface

--- grafo_brendoniano/GrafoBrendoniano.mjs
+++ grafo_brendoniano/GrafoBrendoniano.mjs
@@ -39,6 +39,7 @@ export class GrafoBrendoniano {
 
   addAresta(de_id, para_id, peso) {
     // console.log(this.nodes.find(node => node.id == de_id))
+    this.arestas.push({ de: de_id, para: para_id, peso })
     this.nodes.find(node => node.id == de_id).addFilho(this.nodes.find(node => node.id == para_id), peso)
   }
 
@@ -56,13 +57,14 @@ export class GrafoBrendoniano {
   getDijkstra(node_de) {
     this.setAllNaoVisitado()
     this.setCustoInfinitoAll()
-    this.nodes[node_de].custo = 0;
-    this.nodes[node_de].closed = true;
-    this.ordeneNodes()
-    let menor = this.nodes[0]
+    let nodes = this.nodes
+    nodes.find(node => node.id == node_de).custo = 0;
+    nodes.find(node => node.id == node_de).closed = true;
+    nodes = this._ordeneNodes(nodes)
+    let menor = nodes[0]
 
     while (menor) {
-      this.nodes.filter(node => node.closed).forEach(node_visitado => {
+      nodes.filter(node => node.closed).forEach(node_visitado => {
         node_visitado.filhos.filter(filho => !filho.node.closed).forEach(filho => {
           let soma = node_visitado.custo + filho.peso
           if (soma < filho.node.custo) {
@@ -73,48 +75,50 @@ export class GrafoBrendoniano {
       })
 
 
-      this.ordeneNodes()
-      menor = this.nodes.filter(node => !node.closed)[0]
+      nodes = this._ordeneNodes(nodes)
+      menor = nodes.filter(node => !node.closed)[0]
       if (menor) {
-        this.nodes.filter(node => !node.closed)[0].closed = true
+        nodes.filter(node => !node.closed)[0].closed = true
       }
     }
-    return this.nodes
+    return nodes
   }
 
-  getKahn(){
+  getKahn() {
     return this._getKahn(this.nodes)
   }
-  _getKahn(nodes){
+  _getKahn(nodes) {
     var ordenado = []
     let index = nodes.findIndex(node => node.pais.length == 0)
-    if(index > -1){
+    if (index > -1) {
       var sem_pais = nodes.splice(index, 1)[0]
     }
-    // console.log(sem_pais)
+    console.log(sem_pais)
     while (sem_pais) {
-    sem_pais.filhos.forEach(filho => {
-      // console.log(filho)
-      index = filho.node.pais.findIndex(pai => pai.id == sem_pais.id)
-      if(index > -1){
-        filho.node.pais.splice(index, 1)
-      }
-      // console.log(filho)
-      // console.log("SEPARAÇÃOOOO")
-      
-    })
-    ordenado.push(sem_pais)
+      sem_pais.filhos.forEach(filho => {
+        // console.log(filho)
+        index = filho.node.pais.findIndex(pai => pai.id == sem_pais.id)
+        if (index > -1) {
+          filho.node.pais.splice(index, 1)
+        }
+        // console.log(filho)
+        // console.log("SEPARAÇÃOOOO")
+
+      })
+      ordenado.push(sem_pais)
       index = nodes.findIndex(node => node.pais.length == 0)
-      if(index > -1){
+      if (index > -1) {
         sem_pais = nodes.splice(index, 1)[0]
-      }else{
+      } else {
         sem_pais = false
       }
     }
     return ordenado
   }
   buscaProfundidade(chave, inicio_id) {
-    return this._buscaProfundidade(chave, this.nodes[inicio_id], { status: false, r: "não encontreado" }, 0)
+    this.setAllNaoVisitado()
+
+    return this._buscaProfundidade(chave, this.nodes.find(node => node.id == inicio_id), { status: false, r: "não encontreado" }, 0)
   }
 
   _buscaProfundidade(chave, node, resultado, camada) {
@@ -133,7 +137,9 @@ export class GrafoBrendoniano {
   }
 
   buscaLargura(chave, inicio_id) {
-    let U = this.nodes[inicio_id]
+    this.setAllNaoVisitado()
+
+    let U = this.nodes.find(node => node.id == inicio_id)
     let fila = new Fila()
     fila.entraFila(U)
     return this._buscaLargura(chave, fila, { status: false, r: "não encontreado" }, 0)
@@ -152,11 +158,20 @@ export class GrafoBrendoniano {
         }
         if (!resultado.status) {
           // console.log(U.ant.filhos);
-          U.ant.filhos.map(V => {
-            if (!V.closed) {
-              fila.entraFila(V.node)
-            }
-          })
+          if (U.ant) {
+            U.ant.filhos.map(V => {
+              if (!V.closed) {
+                fila.entraFila(V.node)
+              }
+            })
+          } else {
+            U.filhos.map(V => {
+              if (!V.closed) {
+                fila.entraFila(V.node)
+              }
+            })
+
+          }
           camada++
 
           resultado = this._buscaLargura(chave, fila, resultado, camada)
@@ -177,6 +192,23 @@ export class GrafoBrendoniano {
     return resultado
   }
 
+  getKruskal() {
+    let _nodes = []
+    let _arestas = []
+
+    this.nodes.map(node => {
+      _nodes.push({ node, filhos: [] })
+    })
+
+    _arestas = this.arestas.sort((a, b) => a.peso - b.peso)
+
+    return this._getKruskal(_grafo, _arestas)
+  }
+  _getKruskal(grafo, arestas) {
+    let aresta = arestas.splice(0, 1)
+    console.log(aresta);
+  }
+
   getNaoVisitado() {
     for (var i = 0; i < this.nodes.length; i++) {
       if (!this.nodes[i].closed) {
@@ -194,6 +226,11 @@ export class GrafoBrendoniano {
 
   ordeneNodes() {
     this.nodes = this.nodes.sort((a, b) => {
+      return a.custo - b.custo
+    })
+  }
+  _ordeneNodes(nodes) {
+    return nodes.sort((a, b) => {
       return a.custo - b.custo
     })
   }
--- grafo_brendoniano/index.mjs
+++ grafo_brendoniano/index.mjs
@@ -22,12 +22,91 @@ import { NodoBrendoniano } from './NodoBrendoniano.mjs'
 
 var _grafo = new GrafoBrendoniano()
 export const grafo = {
+    get_grafo() {
+        return _grafo
+    },
     add_nodo(nomeNodo, idNodoPai, pesoNodo) {
         let nodo = new NodoBrendoniano(1, nomeNodo)
         _grafo.addNode(nodo, idNodoPai, pesoNodo)
         console.log(_grafo);
         return _grafo
+    },
+    add_aresta(de_id, para_id, peso) {
+        _grafo.addAresta(de_id, para_id, peso)
+        console.log(_grafo);
+        return _grafo
+    },
+    busca_largura(chave) {
+        let nodo = _grafo.buscaLargura(chave, 0)
+        console.log(nodo);
+        return nodo
+    },
+    busca_profundidade(chave) {
+        let nodo = _grafo.buscaProfundidade(chave, 0)
+        console.log(nodo);
+        return nodo
+    },
+    get_dijkstra(de_id, para_id) {
+        let grafo = _grafo.getMelhorCaminhoDijkstra(de_id, para_id)
+        console.log(grafo);
+        return grafo
+    },
+    get_kahn() {
+        let grafo = _grafo.getKahn()
+        console.log(grafo);
+        return grafo
+    },
+
+    cria_grafo_aleatorio(labels = []) {
+        let tamanho = labels.length
+        for (let i = 0; i < tamanho; i++) {
+            _grafo.addNode(new NodoBrendoniano(0, labels[i]+_grafo.nodes.length))
+        }
+        for (let i = 1; i < _grafo.nodes.length; i++) {
+            for (let j = 0; j < 4; j++) {
+                _grafo.addAresta(Math.floor(Math.random() * _grafo.nodes.length), i, parseInt(Math.random() * 100));
+                // _grafo.addAresta(Math.floor(Math.random() * _grafo.nodes.length), Math.floor(Math.random() * _grafo.nodes.length), parseInt(Math.random() * 100));
+                sleep(1000)
+            }
+        }
+        return _grafo
     }
+
+}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
+// function cria_grafo_aleatorio(tamanho = 0) {
+//     for (let i = 0; i <= tamanho; i++) {
+//         _grafo.addNode(new NodoBrendoniano(0, Math.random() * tamanho))
+//     }
+//     for (let i = 0; i < tamanho; i++) {
+//         for (let i = 0; i < 3; i++) {
+//             _grafo.addAresta(Math.floor(Math.random() * _grafo.nodes.length),Math.floor(Math.random() * _grafo.nodes.length), Math.random()*100);
+//         }
+//     }
+// }
+// cria_grafo_aleatorio(10)
+// let nodo = new NodoBrendoniano(1, "a")
+// _grafo.addNode(nodo)
+// _grafo.addNode(new NodoBrendoniano(0, 'b'), null, null)
+// _grafo.addNode(new NodoBrendoniano(0, 'c'), null, null)
+// _grafo.addNode(new NodoBrendoniano(0, 'd'), null, null)
+// _grafo.addNode(new NodoBrendoniano(0, 'e'), null, null)
+
+
+// _grafo.addAresta(0, 1, null)
+// _grafo.addAresta(0, 2, null)
+// _grafo.addAresta(2, 1, null)
+// _grafo.addAresta(2, 3, null)
+// _grafo.addAresta(1, 3, null)
+// _grafo.addAresta(4, 0, null)
+// _grafo.addAresta(3, 4, null)
+// _grafo.addAresta(1, 3, null)
+
+// let resultado = _grafo.buscaLargura("c", 0)
+// console.log(resultado);
+// resultado = _grafo.buscaProfundidade("c", 0)
+// console.log(resultado);
